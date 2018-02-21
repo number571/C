b92a3311a13b013d7a2183075f00f68d1bfaec71
[#571] <31502293+Number571@users.noreply.github.com>
2018-02-22 06:12:19 +0700

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+long factorial(int x);
+
+int main(void) {
+	int number; scanf("%d", &number);
+	printf("%d\n",factorial(number));
+	return 0;
+}
+
+long factorial(int x) {
+	if (x < 1)
+		return 1;
+	else
+		return x * factorial(x-1);
+}
