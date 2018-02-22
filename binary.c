15a9f85e1ad5eae16a3a56f37faa4e183f28350e
[#571] <31502293+Number571@users.noreply.github.com>
2018-02-22 07:39:31 +0700

    Update binary.c

--- binary.c
+++ binary.c
@@ -7,12 +7,7 @@ int main(void) {
 	return 0;
 }
 void bin(int num) {
-	printf("%d",  (num&0x80)?1:0); // 0x80 = 10000000
-	printf("%d",  (num&0x40)?1:0); // 0x40 = 01000000
-	printf("%d",  (num&0x20)?1:0); // 0x20 = 00100000
-	printf("%d",  (num&0x10)?1:0); // 0x10 = 00010000
-	printf("%d",  (num&0x08)?1:0); // 0x08 = 00001000
-	printf("%d",  (num&0x04)?1:0); // 0x04 = 00000100
-	printf("%d",  (num&0x02)?1:0); // 0x02 = 00000010
-	printf("%d\n",(num&0x01)?1:0); // 0x01 = 00000001
+	for (short bit = 0x80; bit > 0; bit /= 2) {
+		printf("%d",(num&bit)?1:0);
+	} printf("\n");
 }
