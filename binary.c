296fe970cc201d1ae37f7b397ef1e6a8a046dfd0
[#571] <31502293+Number571@users.noreply.github.com>
2018-03-04 05:45:59 +0700

    Update binary.c

--- binary.c
+++ binary.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-void bin(int num);
+void bin(char num);
+
 int main(void) {
-	char number;
-	scanf("%hhd",&number);
-	bin(number);
+	char number; scanf("%hhd",&number); bin(number);
 	return 0;
 }
-void bin(int num) {
+
+void bin(char num) {
 	for (short bit = 0x80; bit > 0; bit /= 2) {
-		printf("%d",(num&bit)?1:0);
+		printf("%hhd",(num&bit)?1:0);
 	} printf("\n");
 }
