d457bf77ad520e6c1f67049d66fc0e8e659700b9
[#571] <31502293+Number571@users.noreply.github.com>
2018-03-04 05:54:11 +0700

    Update xor.c

--- Cryptography/xor.c
+++ Cryptography/xor.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 #include <string.h>
+
 #define BUFFER 128
+
 char *encryptDecrypt(short key, char *message);
+
 int main(void) {
 	short key; scanf("%hd ", &key);
 	char text[BUFFER], temp;
+	
 	for (unsigned short index = 0; index < BUFFER; index++)
 		if ((temp = getchar()) != '\n')
 			text[index] = temp;
 		else break;
+	
 	printf("Final message: %s\n", encryptDecrypt(key, text));
 	return 0;
 }
+
 char *encryptDecrypt(short key, char *message) {
 	for (short index = 0; index < strlen(message); index ++) {
 		message[index] = message[index] ^ key;
