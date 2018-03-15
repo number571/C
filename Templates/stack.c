bf43434409a453e13f0fb615fbb00f8e608c68fa
[#571] <31502293+Number571@users.noreply.github.com>
2018-03-15 20:01:49 +0700

    Create stack.c

---
+++ Templates/stack.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#define MAX 100
+
+void push(int symbol);
+int pop(void);
+
+int buffer[MAX], stack = 0;
+
+int main(void) {
+	push(3); printf("%d\n", pop());
+	return 0;
+}
+
+void push(int symbol) {
+	if (stack < MAX) 
+		buffer[stack++] = symbol;
+	else 
+		printf("Error: stack overflow!\n");
+}
+
+int pop(void) {
+	if (stack > 0) 
+		return buffer[--stack]; 
+	else 
+		printf("Error: stack = 0!\n");
+}
