1f0bfdf8a4ff45130b22213d533bd7169abe2527
[#571] <31502293+Number571@users.noreply.github.com>
2018-05-21 09:42:07 +0700

    Update struct.c

--- Templates/struct.c
+++ Templates/struct.c
@@ -12,7 +12,7 @@ _Bool read(const char *filename);
 int main(void) {
 	const char *name = "file.dat";
 	const struct person id_1 = {"Tom", 26};
-	
+
 	save(name, &id_1);
 	read(name);
 	return 0;
@@ -21,11 +21,11 @@ int main(void) {
 _Bool save(const char *filename, const struct person *ptr) {
 	FILE *file = fopen(filename, "wb");
 	if (file != NULL) {
-		char *c = (char*)ptr;
-		int size = sizeof(struct person);
-		for (int index = 0; index < size; index++) {
-			putc(*c++, file);
-		}
+		for (
+			char *c = (char*)ptr; 
+			c < (char*)ptr + sizeof(struct person); 
+			putc(*c++, file)
+		);
 		fclose(file);
 	} else return 1;
 	return 0;
@@ -36,8 +36,10 @@ _Bool read(const char *filename) {
 	if (file != NULL) {
 		int size = sizeof(struct person) * sizeof(char);
 		struct person *ptr = (struct person*) malloc(size);
+		
 		char *c = (char*)ptr;
 		while ((*c++ = getc(file)) != EOF);
+
 		printf("%s %d\n", ptr->name, ptr->age);
 		free(ptr);
 	} else return 1;
