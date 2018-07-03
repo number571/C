2501589a0c889d5889161fd6c8499e91a5044751
[#571] <31502293+Number571@users.noreply.github.com>
2018-07-03 20:47:02 +0700

    Update caesar.c

--- Cryptography/caesar.c
+++ Cryptography/caesar.c
@@ -10,14 +10,14 @@ int main(void) {
     char mode; short key;
     scanf("%c %hd ", &mode, &key);
 
+    mode = toupper(mode);
     char text[BUFFER], temp;
     for (unsigned short index = 0; index < BUFFER; index++)
         if ((temp = getchar()) != '\n')
             text[index] = temp;
         else break;
 
-    mode = toupper(mode);
-    for (short index = 0; index < strlen(text); index++)
+    for (unsigned short index = 0; index < strlen(text); index++)
         text[index] = toupper(text[index]);
 
     encryptDecrypt(mode, text, key);
@@ -28,9 +28,9 @@ int main(void) {
 
 char *encryptDecrypt(char mode, char *message, short key) {
     if (mode == 'E')
-        for (short index = 0; index < strlen(message); index++)
-            message[index] = (*(message + index) + key - 13) % 26 + 'A';
+        for (unsigned short index = 0; index < strlen(message); index++)
+            message[index] = (message[index] + key - 13) % 26 + 'A';
     else
-        for (short index = 0; index < strlen(message); index++)
-            message[index] = (*(message + index) - key - 13) % 26 + 'A';
+        for (unsigned short index = 0; index < strlen(message); index++)
+            message[index] = (message[index] - key - 13) % 26 + 'A';
 }
