db9b8e6c161ba2d3e90b2fa3f31f0daf3ebd623c
[#571] <31502293+Number571@users.noreply.github.com>
2018-07-04 12:48:45 +0700

    Update byte.c

--- Templates/byte.c
+++ Templates/byte.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
 
+typedef unsigned char uint8_t;
+
 union code {
-	int number;
-	struct {
-		unsigned _0: 1;
-		unsigned _1: 1;
-		unsigned _2: 1;
-		unsigned _3: 1;
-		unsigned _4: 1;
-		unsigned _5: 1;
-		unsigned _6: 1;
-		unsigned _7: 1;
-	} byte;
+    uint8_t number;
+    struct {
+        unsigned _0: 1;
+        unsigned _1: 1;
+        unsigned _2: 1;
+        unsigned _3: 1;
+        unsigned _4: 1;
+        unsigned _5: 1;
+        unsigned _6: 1;
+        unsigned _7: 1;
+    } byte;
 };
 
 int main(void) {
-	union code check;
-	check.number = 5;
+    union code check;
+    check.number = 22;
 
-	printf("%d %d %d %d %d %d %d %d\n", 
-		check.byte._7, check.byte._6, check.byte._5, check.byte._4,
-		check.byte._3, check.byte._2, check.byte._1, check.byte._0);
+    printf("%d %d %d %d %d %d %d %d\n", 
+        check.byte._7, check.byte._6, check.byte._5, check.byte._4,
+        check.byte._3, check.byte._2, check.byte._1, check.byte._0);
 
     return 0;
 }
