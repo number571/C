38c1dc021173c0c50178ce7ec64c48c1a334a853
[#571] <31502293+Number571@users.noreply.github.com>
2018-07-07 17:55:39 +0700

    Update stack.c

--- Templates/stack.c
+++ Templates/stack.c
@@ -7,20 +7,28 @@ int pop(void);
 int buffer[MAX], stack = 0;
 
 int main(void) {
-	push(3); printf("%d\n", pop());
-	return 0;
+
+    push(3); 
+    push(5);
+    push(7);
+
+    printf("%d\n", pop());
+    printf("%d\n", pop());
+    printf("%d\n", pop());
+
+    return 0;
 }
 
 void push(int symbol) {
-	if (stack < MAX) 
-		buffer[stack++] = symbol;
-	else 
-		printf("Error: stack overflow!\n");
+    if (stack < MAX) 
+        buffer[stack++] = symbol;
+    else 
+        printf("Error: stack overflow!\n");
 }
 
 int pop(void) {
-	if (stack > 0) 
-		return buffer[--stack]; 
-	else 
-		printf("Error: stack = 0!\n");
+    if (stack > 0) 
+        return buffer[--stack]; 
+    else 
+        printf("Error: stack = 0!\n");
 }
