6794f377a2a9c9603a081ef2a82d41e6985ec718
[#571] <31502293+Number571@users.noreply.github.com>
2018-07-07 17:48:01 +0700

    Update copy_file.c

--- Templates/copy_file.c
+++ Templates/copy_file.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #define BUFF 256
 
-_Bool copyFile(const char *readfile, const char *copyfile);
+_Bool copyFile(const char* const readfile, const char* const copyfile);
 
 int main(void) {
-	copyFile("file.txt", "hello.txt");
-	return 0;
+    copyFile("file.txt", "hello.txt");
+    return 0;
 }
 
-_Bool copyFile(const char *readfile, const char *copyfile) {
-	char localBuffer[BUFF];
-	FILE *read = fopen(readfile, "r"); 
-	FILE *copy = fopen(copyfile, "w");
-	if ((read != NULL) && (copy != NULL)) {
-		while(fgets(localBuffer, BUFF, read) != NULL)
-			fputs(localBuffer, copy);
-	} else return 1;
-	return 0;
+_Bool copyFile(const char* const readfile, const char* const copyfile) {
+    char localBuffer[BUFF];
+    FILE *read = fopen(readfile, "r"); 
+    FILE *copy = fopen(copyfile, "w");
+    if ((read != NULL) && (copy != NULL)) {
+        while(fgets(localBuffer, BUFF, read) != NULL)
+            fputs(localBuffer, copy);
+    } else return 1;
+    return 0;
 }
