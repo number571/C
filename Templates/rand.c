5d9324ec2219a0a83294f1d881a67095eaf094bf
[#571] <31502293+Number571@users.noreply.github.com>
2018-07-14 18:09:31 +0700

    Create rand.c

---
+++ Templates/rand.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+unsigned long int next = 1;
+
+int rand (void);
+void srand (unsigned int seed);
+
+int main (void) {
+    srand(3);
+
+    printf("%d\n", rand() % 100);
+    printf("%d\n", rand() % 100);
+    printf("%d\n", rand() % 100);
+    
+    return 0;
+}
+
+int rand (void) {
+    next = next * 1103515245 * 12345;
+    return (unsigned int) (next / 65536) % 32768;
+}
+
+void srand (unsigned int seed) {
+    next = seed;
+}
