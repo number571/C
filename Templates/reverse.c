d282b43c1144ecd6861ebad74797b4f57a893bad
[#571] <31502293+Number571@users.noreply.github.com>
2018-07-21 09:11:38 +0700

    Create reverse.c

---
+++ Templates/reverse.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int strlen_ (char str[]);
+void reverse (char s[]);
+
+int main (void) {
+    char string[] = "hello, world";
+    reverse(string);
+    printf("%s\n", string);
+    return 0;
+}
+
+void reverse (char s[]) {
+    int c, i, j;
+    for (i = 0, j = strlen_(s)-1; i < j; i++, j--) {
+        c = s[i]; 
+        s[i] = s[j]; 
+        s[j] = c;
+    }
+}
+
+int strlen_ (char str[]) {
+    int i = 0;
+    while (str[i++] != '\0');
+    return i-1;
+}
