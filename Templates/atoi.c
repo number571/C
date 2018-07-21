0fc03e8cfa019fa7c295d30da443f322d100a8bd
[#571] <31502293+Number571@users.noreply.github.com>
2018-07-21 08:52:11 +0700

    Update atoi.c

--- Templates/atoi.c
+++ Templates/atoi.c
@@ -1,17 +1,34 @@
 #include <stdio.h>
 
-int atoi (char str[]);
+int isdigit_ (char c);
+int isspace_ (char c);
+int atoi_ (char s[]);
 
 int main (void) {
-    char num_string[] = "123";
-    printf("%d\n", atoi(num_string));
+    printf("%d\n", atoi_("-571"));
     return 0;
 }
 
-int atoi (char str[]) {
-    int i, n;
-    i = n = 0;
-    while (str[i] >= '0' && str[i] <= '9')
-        n = 10 * n + (str[i++] - '0');
-    return n;
+int atoi_ (char s[]) {
+    int i, n, sign;
+    for (i = 0; isspace_(s[i]); i++);
+    sign = (s[i] == '-') ? -1 : 1;
+    if (s[i] == '+' || s[i] == '-')
+        i++;
+    for (n = 0; isdigit_(s[i]); i++)
+        n = 10 * n + (s[i] - '0');
+    return sign * n;
+}
+
+int isspace_ (char c) {
+    switch (c) {
+        case ' ': case '\n': case '\t': return 1;
+        default: return 0;
+    }
+}
+
+int isdigit_ (char c) {
+    if (c >= '0' && c <= '9')
+        return 1;
+    else return 0;
 }
