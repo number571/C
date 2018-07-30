82d1d8613dfadfb4352b0895a58d7015bbde0494
[#571] <31502293+Number571@users.noreply.github.com>
2018-07-30 17:09:49 +0700

    Update check_main_dir.c

--- Automation/CreateOnionSite/check_main_dir.c
+++ Automation/CreateOnionSite/check_main_dir.c
@@ -7,13 +7,13 @@ extern char array_changes[QUAN];
 extern unsigned char position;
 
 extern void check_main_dir (void);
-static char check_exist_dir (char *dirname);
-static char check_exist_file (char *filename);
+static char check_exist_dir (const char* const dirname);
+static char check_exist_file (const char* const filename);
 
 extern void check_main_dir (void) {
-    auto char exist_main_dir = check_exist_dir(MAIN_DIR);
-    auto char exist_hostname = check_exist_file(HOST_FILE);
-    auto char exist_private_key = check_exist_file(KEY_FILE);
+    auto const char exist_main_dir = check_exist_dir(MAIN_DIR);
+    auto const char exist_hostname = check_exist_file(HOST_FILE);
+    auto const char exist_private_key = check_exist_file(KEY_FILE);
 
     printf("[D_%s] => %s\n", CHECK_EXIST(exist_main_dir), MAIN_DIR);
     printf("[F_%s] => %s\n", CHECK_EXIST(exist_main_dir), HOST_FILE);
@@ -23,16 +23,16 @@ extern void check_main_dir (void) {
     array_changes[position++] = exist_private_key;
 }
 
-static char check_exist_dir (char *dirname) {
-    auto DIR *dir = opendir(dirname);
+static char check_exist_dir (const char* const dirname) {
+    auto DIR* const dir = opendir(dirname);
     if (dir != NULL) {
         closedir(dir);
         return READABLE;
     } else return UNREADABLE;
 }
 
-static char check_exist_file (char *filename) {
-    auto FILE *file = fopen(filename, "r");
+static char check_exist_file (const char* const filename) {
+    auto FILE* const file = fopen(filename, "r");
     if (file != NULL) {
         fclose(file);
         return READABLE;
