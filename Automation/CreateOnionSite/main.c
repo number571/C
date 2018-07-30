6f6e0a6e43c615978862851e0f2f4cf2a7ee1481
[#571] <31502293+Number571@users.noreply.github.com>
2018-07-30 15:35:28 +0700

    Create main.c

---
+++ Automation/CreateOnionSite/main.c
@@ -0,0 +1,25 @@
+#include "library.h"
+
+extern void create_onion (void);
+extern void check_torrc (void);
+extern void check_main_dir (void);
+extern void edit_readme (void);
+
+/* From sys_call.c */
+extern void start_tor_service (void);
+extern void run_server (void);
+
+char array_changes[QUAN];
+unsigned char position = 0;
+
+int main (void) {
+
+    create_onion();
+    check_torrc();
+    check_main_dir();
+    start_tor_service();
+    edit_readme();
+    run_server();
+
+    return 0;
+}
