572c7fec6f960ca53882d8b515e2200d15bb13fe
[#571] <31502293+Number571@users.noreply.github.com>
2018-07-31 00:59:02 +0700

    Update reverse.c

--- Templates/reverse.c
+++ Templates/reverse.c
@@ -12,11 +12,8 @@ int main (void) {
 
 void reverse (char s[]) {
     int c, i, j;
-    for (i = 0, j = strlen_(s)-1; i < j; i++, j--) {
-        c = s[i]; 
-        s[i] = s[j]; 
-        s[j] = c;
-    }
+    for (i = 0, j = strlen_(s)-1; i < j; i++, j--)
+        c = s[i], s[i] = s[j], s[j] = c;
 }
 
 int strlen_ (char str[]) {
