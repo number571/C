8c0eff49db38a48110490a3dbc09c7986b6007e9
[#571] <31502293+Number571@users.noreply.github.com>
2018-08-25 23:29:45 +0700

    Create right_shift.c

---
+++ Templates/right_shift.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#define N 10U
+
+void right_shift (int arr[], unsigned len);
+void print_array (int arr[], unsigned len);
+
+int main (void) {
+    int array[N] = {1,2,3,4,5,6,7,8,9,10};
+    unsigned i;
+    for (i = 0; i < 3; i++)
+        right_shift(array, N);
+    print_array(array, N);
+    return 0;
+}
+
+void right_shift (int arr[], unsigned len) {
+    int temp = arr[len-1];
+    unsigned i;
+    for (i = N-1; i > 0; i--)
+        arr[i] = arr[i-1];
+    arr[0] = temp;
+}
+
+void print_array (int arr[], unsigned len) {
+    unsigned i;
+    for (i = 0; i < len; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
