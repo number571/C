3f3e2d7c3de11726d116e2219af3466452efb572
[#571] <31502293+Number571@users.noreply.github.com>
2018-09-09 06:50:06 +0700

    Update library.h

--- Programs/SeaBattle/library.h
+++ Programs/SeaBattle/library.h
@@ -4,6 +4,20 @@ typedef enum {false, true} bool;
 
 #define SET_BLOCK '#'
 #define SPACE ' '
+#define BLOCK '@'
+
+#define TARGET '+'
+#define HIT  'X' 
+#define MISS '~'
 
 #define MAP_X 52
 #define MAP_Y 11
+
+#define DEFAULT_X 3
+#define DEFAULT_Y 1
+
+#define DEFAULT_X_OPP 32
+#define DEFAULT_Y_OPP 1
+
+#define MENU_X 16
+#define MENU_Y 6
