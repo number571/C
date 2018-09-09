859078a2728761eb3618eb75d8f9934e4720f1ce
[#571] <31502293+Number571@users.noreply.github.com>
2018-09-09 20:02:26 +0700

    Update main.c

--- Automation/CreateOnionSite/main.c
+++ Automation/CreateOnionSite/main.c
@@ -1,27 +1,8 @@
-#include "library.h"
-
-/* [Example]: $ make -> $ sudo ./main */
-
-extern void create_onion (void);
-extern void check_torrc (void);
-extern void check_main_dir (void);
-extern void edit_readme (void);
-
-/* From sys_call.c */
-extern void start_tor_service (void);
-extern void run_server (void);
-
-char array_changes[QUAN];
-unsigned char position = 0;
+#include "extern_func.h"
 
 int main (void) {
 
-    create_onion();
-    check_torrc();
-    check_main_dir();
-    start_tor_service();
-    edit_readme();
-    run_server();
-
+    activate();
+    
     return 0;
 }
