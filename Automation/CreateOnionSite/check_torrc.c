41602c7e2d49c487113fab25fed7fb2e6498b61f
[#571] <31502293+Number571@users.noreply.github.com>
2018-09-09 20:03:45 +0700

    Update check_torrc.c

--- Automation/CreateOnionSite/check_torrc.c
+++ Automation/CreateOnionSite/check_torrc.c
@@ -6,26 +6,12 @@
 extern char array_changes[QUAN];
 extern unsigned char position;
 
-extern void check_torrc (void);
-static char edit_torrc  (struct List *st_torrc);
-
-extern void check_torrc (void) {
-    auto struct List torrc  = { UNREADABLE, TORRC_PATH };
-
-    edit_torrc(&torrc); 
-    printf("[F_%s] => %s\n", CHECK_MODE(torrc.mode), torrc.path);
-    if (torrc.mode == OVERWRITTEN)
-        printf("| => Lines [ADDED] => %s\n", torrc.path);
-
-    array_changes[position++] = torrc.mode;
-}
-
 static char edit_torrc (struct List *st_torrc) {
-    auto struct Data hsd = { false, HIDDEN_SERVICE_DIR  "\n"};
-    auto struct Data hsp = { false, HIDDEN_SERVICE_PORT "\n"};
+    struct Data hsd = { false, HIDDEN_SERVICE_DIR  "\n" };
+    struct Data hsp = { false, HIDDEN_SERVICE_PORT "\n" };
 
-    auto char buffer[BUFF];
-    auto FILE *torrc;
+    char buffer[BUFF];
+    FILE *torrc;
 
     if ((torrc = fopen(st_torrc->path, "r")) != NULL) {
         while (fgets(buffer, BUFF, torrc) != NULL) {
@@ -48,4 +34,15 @@ static char edit_torrc (struct List *st_torrc) {
     }
 
     return st_torrc->mode;
+}
+
+extern void check_torrc (void) {
+    struct List torrc  = { UNREADABLE, TORRC_PATH };
+
+    edit_torrc(&torrc); 
+    printf("[F_%s] => %s\n", CHECK_MODE(torrc.mode), torrc.path);
+    if (torrc.mode == OVERWRITTEN)
+        printf("| => Lines [ADDED] => %s\n", torrc.path);
+
+    array_changes[position++] = torrc.mode;
 }
