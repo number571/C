b9a845d138697d2e5eb2811d1fe1d88bc9976dd7
[#571] <31502293+Number571@users.noreply.github.com>
2018-09-11 10:12:37 +0700

    Update atbash.c

--- Cryptography/atbash.c
+++ Cryptography/atbash.c
@@ -1,34 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-char *atbash (const char * const message);
+void atbash (char to[], char from[]);
 char char_atbash (const char ch);
 
 int main(void) {
-    printf("%s\n", atbash("SVOOL, DLIOW"));
+    char message[] = "HELLO, WORLD";
+    atbash(message, message);
+    printf("%s\n", message);
     return 0;
 }
 
-char *atbash (const char * const message) {
-    unsigned long length = strlen(message);
-    unsigned long i;
-
-    char *encrypted_message = NULL;
-    encrypted_message = (char*)malloc(length + 1);
-
-    for (i = 0; i < length; ++i)
-        encrypted_message[i] = char_atbash(message[i]);
-    encrypted_message[i] = '\0';
-
-    return encrypted_message;
+void atbash (char to[], char from[]) {
+    char *p = NULL;
+    for (p = from; *p != '\0'; ++p)
+        *(to + (p - from)) = char_atbash(*p);
+    *(to + (p - from)) = '\0';
 }
 
 char char_atbash (const char ch) {
     char *reversed = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
-
     if ('A' <= ch && ch <= 'Z') 
         return reversed[ch - 'A'];
-
-    else return ch;
+    return ch;
 }
