26cb9d954618cb3ddd2805ea1e3b52874a7c67ee
[#571] <31502293+Number571@users.noreply.github.com>
2018-09-11 10:34:41 +0700

    Update atbash.c

--- Cryptography/atbash.c
+++ Cryptography/atbash.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
+#define END_OF_STRING '\0'
 
-void atbash (char to[], char from[]);
+void atbash (char * const to, char * const from);
 char char_atbash (const char ch);
 
 int main(void) {
     char message[] = "HELLO, WORLD";
+
     atbash(message, message);
     printf("%s\n", message);
+
     return 0;
 }
 
-void atbash (char to[], char from[]) {
+void atbash (char * const to, char * const from) {
     char *p = NULL;
-    for (p = from; *p != '\0'; ++p)
+    for (p = from; *p != END_OF_STRING; ++p)
         *(to + (p - from)) = char_atbash(*p);
-    *(to + (p - from)) = '\0';
+    *(to + (p - from)) = END_OF_STRING;
 }
 
 char char_atbash (const char ch) {
