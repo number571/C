2ac93ca18a90a6f254ad947f373ec825b2b524c8
[#571] <31502293+Number571@users.noreply.github.com>
2018-09-18 05:54:11 +0700

    Create caesar.h

---
+++ Cryptography/crypto/caesar.h
@@ -0,0 +1,47 @@
+#include <string.h>
+#include "expansion.h"
+
+char __alpha_caesar[MAX_CHAR_QUANTITY] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char __length_caesar = LEN_ALPHA;
+
+char _char_caesar (const char key, const char ch) {
+    char *p = NULL;
+    for (p = __alpha_caesar; *p != END_OF_STRING; ++p)
+        if (*p == ch)
+            return __alpha_caesar[(p - __alpha_caesar + key + __length_caesar) % __length_caesar];
+    return ch;
+}
+
+_Bool set_alpha_caesar (char * const alpha) {
+    unsigned int length = strlen(alpha);
+    if (length >= MAX_CHAR_QUANTITY)
+        return 1;
+
+    __length_caesar = (char)length;
+    char *p = NULL;
+
+    for (p = alpha; *p != END_OF_STRING; ++p)
+        __alpha_caesar[p - alpha] = *p;
+    __alpha_caesar[p - alpha] = END_OF_STRING;
+
+    return 0;
+}
+
+_Bool caesar (
+    char * const to, 
+    const char mode, 
+    char key, 
+    char * const from
+) {
+    if (mode != ENCRYPT_MODE && mode != DECRYPT_MODE)
+        return 1;
+
+    char *p = NULL;
+    key = ( (key < 0) ? (__length_caesar + (key % __length_caesar)) : (key % __length_caesar) ) * mode;
+
+    for (p = from; *p != END_OF_STRING; ++p)
+        to[p - from] = _char_caesar(key, *p);
+    to[p - from] = END_OF_STRING;
+
+    return 0;
+}
