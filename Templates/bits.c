bb15cb97597ad957143eb2ed96dc4856dba92149
[#571] <31502293+Number571@users.noreply.github.com>
2018-10-20 00:07:45 +0700

    Update bits.c

--- Templates/bits.c
+++ Templates/bits.c
@@ -1,28 +1,33 @@
 #include <stdio.h>
 
-typedef unsigned char uint8_t;
+typedef union {
+	unsigned char byte;
+	struct {
+		unsigned char _0: 1;
+		unsigned char _1: 1;
+		unsigned char _2: 1;
+		unsigned char _3: 1;
+		unsigned char _4: 1;
+		unsigned char _5: 1;
+		unsigned char _6: 1;
+		unsigned char _7: 1;
+	} bit;
+} Byte;
 
-union code {
-    uint8_t number;
-    struct {
-        unsigned _0: 1;
-        unsigned _1: 1;
-        unsigned _2: 1;
-        unsigned _3: 1;
-        unsigned _4: 1;
-        unsigned _5: 1;
-        unsigned _6: 1;
-        unsigned _7: 1;
-    } bit;
-};
+void print_bits (Byte x) {
+	printf("%hhu%hhu%hhu%hhu%hhu%hhu%hhu%hhu",
+		x.bit._7, x.bit._6, x.bit._5, x.bit._4,
+		x.bit._3, x.bit._2, x.bit._1, x.bit._0);
+}
 
 int main(void) {
-    union code check;
-    check.number = 22;
+	Byte x = {.byte = 5};
+	Byte y = {.byte = 2};
+	Byte z = {.byte = x.byte << y.byte};
 
-    printf("%d %d %d %d %d %d %d %d\n", 
-        check.bit._7, check.bit._6, check.bit._5, check.bit._4,
-        check.bit._3, check.bit._2, check.bit._1, check.bit._0);
+	print_bits(x); printf(" [%hhu]\n", x.byte);
+	print_bits(y); printf(" [%hhu]\n", y.byte);
+	print_bits(z); printf(" [%hhu]\n", z.byte);
 
     return 0;
 }
