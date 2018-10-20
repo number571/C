411ba3d14619f8b80035a8b528724b4d28bfa8d8
[#571] <31502293+Number571@users.noreply.github.com>
2018-10-20 22:15:59 +0700

    Update bacon.c

--- Cryptography/crypto/bacon.c
+++ Cryptography/crypto/bacon.c
@@ -6,91 +6,87 @@
 static char __alpha_bacon[MAX_CHAR_QUANTITY] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 static char __default_char_bacon[2] = {'A', 'B'};
 
-static char _char_bacon (const char ch, const char x) {
-    char *p = NULL;
-    for (p = __alpha_bacon; *p != END_OF_STRING; ++p)
+static char _index_char_bacon (const char ch) {
+    for (char *p = __alpha_bacon; *p != END_OF_STRING; ++p)
         if (*p == ch)
-            return (p - __alpha_bacon) & x ? __default_char_bacon[1] : __default_char_bacon[0];
-    return ch;
+            return p - __alpha_bacon - 1;
+    return -1;
 }
 
-static void _encrypt_bacon (char * const to, char * const from) {
-    unsigned int position = 0;
-    char x, *p = NULL;
-    
-    for (p = from; *p != END_OF_STRING; ++p)
-        for (x = 0x40; x >= 0x01; x /= 2) {
-            to[position++] = _char_bacon(*p, x);
-            if (to[position - 1] != __default_char_bacon[0] && to[position - 1] != __default_char_bacon[1])
-                break;
+static void _encrypt_bacon (char * to, const char * const from) {
+    const unsigned int length = strlen(from);
+
+    char buffer[length + 1];
+    strcpy(buffer, from);
+
+    for (char *p = buffer; *p != END_OF_STRING; ++p) {
+        const char index = _index_char_bacon(*p);
+        if (index == -1) {
+            *to++ = *p;
+            continue;
         }
+        for (char x = 0x40; x >= 0x01; x /= 2)
+            *to++ = __alpha_bacon[index] & x ? __default_char_bacon[1] : __default_char_bacon[0];
+    }
 
-    to[position] = END_OF_STRING;
+    *to = END_OF_STRING;
 }
 
-static unsigned int _from_vector_to_alpha_bacon (
-    char * const to,
-    unsigned int position,
-    char * const position_buffer,
+static char _from_vector_to_alpha_bacon (
+    char * to,
+    const char position,
     const char * const buffer
 ) {
-    char x, sum = 0;
-
-    if (*position_buffer != 0 && *position_buffer % SEVEN_BITS == 0) {
-        for (x = SEVEN_BITS - 1; x > 0; --x)
+    if (position != 0 && position % SEVEN_BITS == 0) {
+    	char sum = 0;
+        for (char x = SEVEN_BITS - 1; x > 0; --x)
             if (buffer[x] == __default_char_bacon[1])
                 sum += (char)pow(2, SEVEN_BITS - 1 - x);
 
-        to[position++] = __alpha_bacon[sum];
-        sum = *position_buffer = 0;
-    } 
-
-    return position;
-}
-
-static void _decrypt_bacon (char * const to, char * const from) {
-    unsigned int position = 0;
-    char position_buffer = 0;
-
-    char x, *p = NULL;
-    char buffer[SEVEN_BITS + 1];
-
-    for (p = from; *p != END_OF_STRING; ++p) {
-        position = _from_vector_to_alpha_bacon(to, position, &position_buffer, buffer);
-
-        if (*p != __default_char_bacon[0] && *p != __default_char_bacon[1]) {
-            to[position++] = *p;
-            continue;
-        }
-
-        buffer[position_buffer++] = *p;
-    }
-
-    position = _from_vector_to_alpha_bacon(to, position, &position_buffer, buffer);
-
-    to[position] = END_OF_STRING;
-}
-
-extern void set_char_bacon (const char first, const char second) {
-    __default_char_bacon[0] = first;
-    __default_char_bacon[1] = second;
-}
-
-extern char set_alpha_bacon (char * const alpha) {
-    if (strlen(alpha) >= MAX_CHAR_QUANTITY)
+        *to++ = __alpha_bacon[sum];
         return 1;
-
-    char *p = NULL;
-    for (p = alpha; *p != END_OF_STRING; ++p)
-        __alpha_bacon[p - alpha] = *p;
-
-    __alpha_bacon[p - alpha] = END_OF_STRING;
+    } 
 
     return 0;
 }
 
-extern char bacon (char * const to, const char mode, char * const from) {
-    switch(mode) {
+static void _decrypt_bacon (char * to, char * from) {
+    char position = 0;
+    char buffer[SEVEN_BITS + 1];
+
+    for (; *from != END_OF_STRING; ++from) {
+        to = _from_vector_to_alpha_bacon(to, position, buffer) ? (to + 1) : (to);
+        position %= SEVEN_BITS;
+
+        if (*from != __default_char_bacon[0] && *from != __default_char_bacon[1]) {
+            *to++ = *from;
+            continue;
+        }
+
+        buffer[position++] = *from;
+    }
+
+    to = _from_vector_to_alpha_bacon(to, position, buffer) ? (to + 1) : (to);
+    position %= SEVEN_BITS;
+
+    *to = END_OF_STRING;
+}
+
+extern void set_char_bacon (const char ch1, const char ch2) {
+    __default_char_bacon[0] = ch1;
+    __default_char_bacon[1] = ch2;
+}
+
+extern char set_alpha_bacon (const char * const alpha) {
+    if (strlen(alpha) >= MAX_CHAR_QUANTITY)
+        return 1;
+
+    strcpy(__alpha_bacon, alpha);
+    return 0;
+}
+
+extern char bacon (char * to, const char mode, char * from) {
+    switch (mode) {
         case ENCRYPT_MODE:
             _encrypt_bacon(to, from); 
         break;
