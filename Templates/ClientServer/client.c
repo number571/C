432ed1d9e434e606afbc3c713632aefd2835a938
[#571] <31502293+Number571@users.noreply.github.com>
2018-10-22 22:43:56 +0700

    Create client.c

---
+++ Templates/ClientServer/client.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define BUFF 512
+
+char buffer[BUFF];
+const char message[100] = "hello, world";
+
+int main (void) {
+    int conn = socket(AF_INET, SOCK_STREAM, 0);
+    if (conn < 0) {
+        fprintf(stderr, "Error: socket\n");
+        return 1;
+    }
+
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(8080);
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if (connect(conn, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+        fprintf(stderr, "Error: connect\n");
+        return 2;
+    }
+
+    send(conn, message, sizeof(message), 0);
+    recv(conn, buffer, BUFF, 0);
+
+    printf("%s\n", buffer);
+    close(conn);
+
+    return 0;
+}
