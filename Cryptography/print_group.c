a71b03713e8856f821beda2b142bbb84ae73e3d1
[#571] <31502293+Number571@users.noreply.github.com>
2019-12-21 00:34:38 -0500

    Update print_group.c

--- Cryptography/print_group.c
+++ Cryptography/print_group.c
@@ -4,10 +4,10 @@
 uint64_t addmod(uint64_t x, uint64_t y, uint64_t n);
 uint64_t mulmod(uint64_t x, uint64_t y, uint64_t n);
 uint64_t powmod(uint64_t x, uint64_t y, uint64_t n);
-void print_group(uint8_t n, uint64_t (*groupf) (uint64_t, uint64_t, uint64_t));
+void print_group(uint64_t n, uint64_t (*groupf) (uint64_t, uint64_t, uint64_t));
 
 int main(void) {
-    print_group(11, powmod);
+    print_group(5, powmod);
     return 0;
 }
 
@@ -31,16 +31,16 @@ uint64_t powmod(uint64_t x, uint64_t y, uint64_t n) {
     return r;
 }
 
-void print_group(uint8_t n, uint64_t (*groupf) (uint64_t, uint64_t, uint64_t)) {
+void print_group(uint64_t n, uint64_t (*groupf) (uint64_t, uint64_t, uint64_t)) {
     printf(" Group: %d\n", n);
-    printf(" - |");
-    for (uint8_t i = 0; i < n; ++i) {
+    printf(" Z |");
+    for (uint64_t i = 0; i < n; ++i) {
         printf("%2d |", i);
     }
     putchar('\n');
-    for (uint8_t i = 1; i < n; ++i) {
+    for (uint64_t i = 1; i < n; ++i) {
         printf("%2d |", i);
-        for (uint8_t j = 0; j < n; ++j) {
+        for (uint64_t j = 0; j < n; ++j) {
             printf("%2d |", (*groupf)(i, j, n));
         }
         putchar('\n');
