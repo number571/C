68a8119767995fe38de8adfb08a20199edd672cb
[#571] <31502293+Number571@users.noreply.github.com>
2020-04-25 01:05:18 -0500

    Update bigint.c

--- Templates/bigint.c
+++ Templates/bigint.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MODULO 1000000000
+
+// // count = 6
+// // size = 6 * 2 = 12 * 2 = 24 ...
+// [ 11, 22, 33, 00, 00, 00 ]
+// 0 - null
+
+// * 2
+// << 1
 
 typedef struct BigInt {
 	uint32_t *number;
@@ -43,13 +51,13 @@ static void _set_count_bigint(BigInt *x, BigInt *y) {
 	if (x->count < y->count) {
 		x->count = y->count;
 	}
-} 
+}
 
 static void _clr_count_bigint(BigInt *x) {
 	if (x->count == 0) {
-		return;
+		return; // null
 	}
-	ssize_t i = x->count-1;
+	size_t i = x->count-1;
 	for (; i > 0; --i) {
 		if (x->number[i] != 0) {
 			break;
@@ -58,25 +66,26 @@ static void _clr_count_bigint(BigInt *x) {
 	x->count = i + 1;
 }
 
-extern BigInt* new_bigint(uint8_t *str);
-extern void    free_bigint(BigInt *x);
+extern BigInt *new_bigint(uint8_t *str);
+extern void free_bigint(BigInt *x);
 
-extern void    add_bigint(BigInt *x, BigInt *y);
-extern void    sub_bigint(BigInt *x, BigInt *y);
+extern void add_bigint(BigInt *x, BigInt *y);
+extern void sub_bigint(BigInt *x, BigInt *y);
 
-extern int8_t  cmp_bigint(BigInt *x, BigInt *y);
-extern void    xchg_bigint(BigInt *x, BigInt *y);
-extern void    print_bigint(BigInt *x);
+extern int8_t cmp_bigint(BigInt *x, BigInt *y);
+extern void xchg_bigint(BigInt *x, BigInt *y);
+
+extern void print_bigint(BigInt *x);
 
 int main(void) {
-	BigInt *x = new_bigint("99999999999999999999999999999999999999999999999999999999999999");
+	BigInt *x = new_bigint("0");
 	BigInt *y = new_bigint("1");
 
-	add_bigint(x, y);
+	sub_bigint(x, y);
 	print_bigint(x);
 
-	free_bigint(y);
 	free_bigint(x);
+	free_bigint(y);
 	return 0;
 }
 
@@ -88,7 +97,7 @@ extern void xchg_bigint(BigInt *x, BigInt *y) {
 
 extern int8_t cmp_bigint(BigInt *x, BigInt *y) {
 	_set_count_bigint(x, y);
-	for (ssize_t i = x->count-1; i != -1; ++i) {
+	for (ssize_t i = x->count-1; i != -1; --i) {
 		if (x->number[i] < y->number[i]) {
 			return -1;
 		}
@@ -104,8 +113,8 @@ extern void print_bigint(BigInt *x) {
 		printf("null\n");
 		return;
 	}
-	ssize_t i = 0;
-	for (i = x->count-1; i > 0; --i) {
+	ssize_t i = x->count-1;
+	for (; i > 0; --i) {
 		if (x->number[i] != 0) {
 			break;
 		}
@@ -119,11 +128,11 @@ extern void print_bigint(BigInt *x) {
 
 extern void add_bigint(BigInt *x, BigInt *y) {
 	_set_count_bigint(x, y);
-	uint32_t carry = 0;
+	uint8_t carry = 0;
 	for (size_t i = 0; i < x->count; ++i) {
-		uint64_t  temp = carry + x->number[i] + y->number[i];
+		uint64_t temp = carry + x->number[i] + y->number[i];
 		x->number[i] = temp % MODULO;
-		carry = (temp >= MODULO) ? 1 : 0;
+		carry = temp / MODULO;
 	}
 	if (carry) {
 		x->number[x->count] = carry;
@@ -139,22 +148,16 @@ extern void sub_bigint(BigInt *x, BigInt *y) {
 		return;
 	}
 	_set_count_bigint(x, y);
-	uint32_t carry = 0;
-	uint64_t temp = 0;
+	uint8_t carry = 0;
 	for (size_t i = 0; i < x->count; ++i) {
-		temp = (MODULO + x->number[i]) - (carry + y->number[i]);
+		uint64_t temp = (MODULO + x->number[i]) - (carry + y->number[i]);
 		x->number[i] = temp % MODULO;
 		carry = (temp >= MODULO) ? 0 : 1;
 	}
 	_clr_count_bigint(x);
 }
 
-extern void free_bigint(BigInt *x) {
-	free(x->number);
-	free(x);
-}
-
-extern BigInt* new_bigint(uint8_t *str) {
+extern BigInt *new_bigint(uint8_t *str) {
 	const size_t BUFFSIZE = 9;
 
 	BigInt *bigint = (BigInt*)malloc(sizeof(BigInt));
@@ -162,12 +165,12 @@ extern BigInt* new_bigint(uint8_t *str) {
 	bigint->count  = 0;
 	bigint->number = (uint32_t*)malloc(bigint->size * sizeof(uint32_t));
 
-	size_t  next = 0;
-	size_t  index = 0;
-	size_t  length = strlen(str);
+	uint8_t chunck[BUFFSIZE+1];
+	memset(chunck, 0, (BUFFSIZE+1) * sizeof(uint8_t));
 
-	uint8_t chunck[BUFFSIZE];
-	memset(chunck, 0, BUFFSIZE * sizeof(uint8_t));
+	size_t next = 0;
+	size_t index = 0;
+	size_t length = strlen(str);
 
 	for (ssize_t i = length-1; i != -1; --i) {
 		chunck[index++] = str[i];
@@ -175,7 +178,7 @@ extern BigInt* new_bigint(uint8_t *str) {
 			index = 0;
 			_swap(chunck, BUFFSIZE);
 			bigint->number[next++] = atoi(chunck);
-			_inc_count_bigint(bigint);
+			_inc_count_bigint(bigint);	
 		}
 	}
 	if (index != 0) {
@@ -185,16 +188,10 @@ extern BigInt* new_bigint(uint8_t *str) {
 		_inc_count_bigint(bigint);
 	}
 
-	uint32_t carry = 0;
-	for (size_t i = 0; i < bigint->count; ++i) {
-		uint64_t temp = carry + bigint->number[i];
-		bigint->number[i] = temp % MODULO;
-		carry = temp / MODULO;
-	}
-	if (carry) {
-		bigint->number[bigint->count] = carry;
-		_inc_count_bigint(bigint);
-	}
-
 	return bigint;
+}
+
+extern void free_bigint(BigInt *x) {
+	free(x->number);
+	free(x);
 }
