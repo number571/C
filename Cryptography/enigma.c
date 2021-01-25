82b420c7f46ef11478e5f41dd1b25b93fd2b1e67
[#571] <31502293+Number571@users.noreply.github.com>
2021-01-25 12:28:11 -0500

    Update enigma.c

--- Cryptography/enigma.c
+++ Cryptography/enigma.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
 #include <string.h>
 
+#define ROTNUM 3
+#define SWTNUM 6
 #define ALPSIZ 26
 
+// A-Z, B-Y, C-X, ...
 static char reflector[ALPSIZ+1] = {
     "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
 };
-static char rotors[3][ALPSIZ+1] = {
+// A-F, B-E, C-D, ...
+static char switches[SWTNUM+1] = {
+    "ABCDEF",
+};
+static char rotors[ROTNUM][ALPSIZ+1] = {
     "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
     "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
     "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
 };
+static int shfrot[ROTNUM] = {
+    1, 2, 3,
+};
 
-char *encrypt(char *output, const char *input, int size);
-int find_index(char *rotor, char ch);
-void shift_rotor(char *rotor, int size);
+extern char *encrypt(char *output, const char *input, int size);
+static int find_index(char *rotor, char ch, int size);
+static void shift_rotor(char *rotor, int size);
 
 int main(int argc, char const *argv[]) {
     int len;
@@ -29,39 +39,52 @@ int main(int argc, char const *argv[]) {
     return 0;
 }
 
-char *encrypt(char *output, const char *input, int size) {
-    int rot1, rot2, rot3;
+extern char *encrypt(char *output, const char *input, int size) {
+    int swt, temp;
+
     for (int i = 1; i < size+1; ++i) {
-        rot1 = input[i-1];
+        temp = input[i-1];
 
-        rot1 = rotors[0][rot1 - 'A'];
-        rot2 = rotors[1][rot1 - 'A'];
-        rot3 = rotors[2][rot2 - 'A'];
-
-        rot3 = ALPSIZ - 1 - find_index(reflector, rot3);
-        rot3 = find_index(reflector, rot3 + 'A');
-
-        rot3 = find_index(rotors[2], rot3 + 'A');
-        rot2 = find_index(rotors[1], rot3 + 'A');
-        rot1 = find_index(rotors[0], rot2 + 'A');
-
-        output[i-1] = rot1 + 'A';
-
-        if (i % 1 == 0) {
-            shift_rotor(rotors[0], ALPSIZ);
+        // Switches
+        swt = find_index(switches, temp, SWTNUM);
+        if (swt != -1) {
+            temp = switches[SWTNUM - swt - 1];
         }
-        if (i % 2 == 0) {
-            shift_rotor(rotors[1], ALPSIZ);
+
+        // Rotors encrypt ->
+        for (int j = 0; j < ROTNUM; ++j) {
+            temp = rotors[j][temp - 'A'];
         }
-        if (i % 3 == 0) {
-            shift_rotor(rotors[2], ALPSIZ);
+        
+        // Reflector
+        temp = ALPSIZ - 1 - find_index(reflector, temp, ALPSIZ);
+
+        // Rotors encrypt <-
+        for (int j = ROTNUM-1; j > -1; --j) {
+            temp = find_index(rotors[j], temp + 'A', ALPSIZ);
+        }
+
+        // Switches
+        temp += 'A';
+        swt = find_index(switches, temp, SWTNUM);
+        if (swt != -1) {
+            temp = switches[SWTNUM - swt - 1];
+        }
+
+        output[i-1] = temp;
+
+        // Shift rotors
+        for (int j = 0; j < ROTNUM; ++j) {
+            if (i % shfrot[j] == 0) {
+                shift_rotor(rotors[j], ALPSIZ);
+            }
         }
     }
     return output;
 }
 
-int find_index(char *rotor, char ch) {
-    for (int j = 0; j < ALPSIZ; ++j) {
+static int find_index(char *rotor, char ch, int size) {
+    for (int j = 0; j < size; ++j) {
         if (rotor[j] == ch) {
             return j;
         }
@@ -69,7 +92,7 @@ int find_index(char *rotor, char ch) {
     return -1;
 }
 
-void shift_rotor(char *rotor, int size) {
+static void shift_rotor(char *rotor, int size) {
     char temp = rotor[size-1];
     for (int i = size-1; i > 0; --i) {
         rotor[i] = rotor[i-1];
